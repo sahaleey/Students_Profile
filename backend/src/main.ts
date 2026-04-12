4036080d10870b2ae5299fdc5efad9c1cde8b1db
sahaleey <ajua46244@gmail.com>
2026-04-12 08:06:28 -0700

    feat: Update CORS origin and README to reflect production URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://nahj-studentsprofile.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
