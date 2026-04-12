c494f0bd5ce294ce84c78d75c5d6f0a939592436
sahaleey <ajua46244@gmail.com>
2026-04-12 08:10:24 -0700

    feat: Update CORS origin to remove trailing slash for consistency

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://nahj-studentsprofile.vercel.app/',
+    origin: 'https://nahj-studentsprofile.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
