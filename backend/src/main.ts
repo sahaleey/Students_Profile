9e75f4c893bafb4c4b179a2ac17af240bd37461e
sahaleey <ajua46244@gmail.com>
2026-04-15 10:15:46 -0700

    feat: update CORS configuration to use environment variable for origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow frontend origin or default to localhost
+    origin: process.env.CORS_ORIGIN, // Allow frontend origin or default to localhost
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
