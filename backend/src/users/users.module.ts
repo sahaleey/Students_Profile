1416594c20c475713e1400a2c393ccea41362b1a
sahaleey <ajua46244@gmail.com>
2026-04-18 02:56:31 -0700

    feat: update CORS configuration to include default origin and add UsersController to UsersModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   // This tells TypeORM to create the User table
   imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
